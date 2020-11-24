20b6dadedf3d5fe6b4acb88a97f72ebdcea3dcba
student20oct <72745037+student20oct@users.noreply.github.com>
2020-11-24 02:29:37 -0500

    Add files via upload

---
+++ Bird.js
@@ -0,0 +1,24 @@
+class Bird extends BaseClass {
+  constructor(x,y){
+    super(x,y,50,50);
+    this.image = loadImage("sprites/bird.png");
+    this.Smokeimage = loadImage("sprites/smoke.png")
+   this.path = [];
+  }
+  
+  display() {
+    //this.body.position.x = mouseX;
+    //this.body.position.y = mouseY;
+    
+    super.display();
+    if(this.body.position.x>200 && this.body.velocity.x>8){
+      var position = [this.body.position.x,this.body.position.y]
+      this.path.push(position);
+    }
+  
+    console.log(this.path);
+    for(var i = 0;i<this.path.length;i=i+1){
+      image(this.Smokeimage,this.path[i][0],this.path[i][1])
+    }
+  }
+}
